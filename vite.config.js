eb04d2d74a08836c011eeda17bd07c4c46c1fd2c
mukuljain810 <42878068+mukuljain810@users.noreply.github.com>
2026-02-16 20:59:23 +0530

    Create vite.config.js with GitHub Pages base URL configuration

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/ultratech_itinerary/',
+  plugins: [vue()]
+});
